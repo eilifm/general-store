9099b4de3f90cc4a3a5bd8a79e262a08aeabc197
eilifm <eilif@realtyshares.com>
2018-10-12 12:53:48 -0700

    ntp monitor improvements

--- ntp_test.py
+++ ntp_test.py
@@ -7,12 +7,26 @@ from scipy.stats import norm
 import tableprint
 from string import Template
 from tabulate import tabulate
+import math
+from subprocess import Popen, PIPE
+
 
 poll = True
 
 
 last_compute = 0
 poll_freq = 3
+
+def weighted_avg_and_std(values, weights):
+    """
+    Return the weighted average and standard deviation.
+
+    values, weights -- Numpy ndarrays with the same shape.
+    """
+    average = np.average(values, weights=weights[-len(values):])
+    # Fast and numerically precise:
+    variance = np.average((values-average)**2, weights=weights[-len(values):])
+    return (average, math.sqrt(variance))
 
 
 
@@ -26,7 +40,12 @@ class NTPMonitor:
         self.poll_samples = poll_samples
         self._max_samples = sample_retention
         self._cert = cert
-        self.poll()
+        self._weights = (np.array(list(range(self._max_samples))) + 1)/50
+
+        process = Popen(['ntpdate', '-u', 'time.google.com'], stdout=PIPE, stderr=PIPE)
+        stdout, stderr = process.communicate()
+
+        self.poll(samples = 6, init=True)
 
     @property
     def last(self) -> ntp.NTPStats:
@@ -34,7 +53,7 @@ class NTPMonitor:
 
     @property
     def mean(self) -> float:
-        return stats.mean([x.offset for x in self.data])
+        return weighted_avg_and_std(np.array([x.offset for x in self.data]), self._weights)[0]
 
     @property
     def rms_offset(self)-> float:
@@ -42,17 +61,23 @@ class NTPMonitor:
 
     @property
     def stdev(self) -> float:
-        return stats.stdev([x.offset for x in self.data])
+        # return stats.stdev([x.offset for x in self.data])
+        return weighted_avg_and_std(np.array([x.offset for x in self.data]), self._weights)[1]
 
     @property
     def delay_stdev(self) -> float:
         return stats.stdev([x.delay for x in self.data])
 
     @property
+    def delay_median(self) -> float:
+        return stats.median([x.delay for x in self.data])
+
+    @property
     def range(self) -> List[float]:
         return [min([x.offset for x in self.data]), max([x.offset for x in self.data])]
 
-    def poll(self, samples = None):
+    def poll(self, samples = None, init = False):
+
         if not samples:
             tmp = [self._client.request(self._server, version=3) for i in range(self.poll_samples)]
 
@@ -64,13 +89,18 @@ class NTPMonitor:
 
         self.last_computed_sys_time = self.data[-1].recv_time
 
-        # Restrict data to last n samples
-        if len(self.data) > self._max_samples:
-            self.data = self.data[-self._max_samples:]
+        if not init:
+            # Restrict data to last n samples
+            if len(self.data) > self._max_samples:
+                self.data = self.data[-self._max_samples:]
 
-        s = self.delay_stdev
-        self.data = list(filter(lambda x: True if x.delay >= s*2 else False, self.data))
+            s = self.delay_stdev
+            mean = self.delay_median
 
+            # NTP Delay Range Filter
+            self.data = list(filter(lambda x: True if x.delay <= (mean+(s*2)) else False, self.data))
+
+            # print(len(self.data))
 
     def NTPStatsToDict(self, ntp_struct: ntp.NTPStats):
         return {'offset': ntp_struct.offset,
@@ -102,24 +132,24 @@ class NTPMonitor:
         return [(now-me), (now+me), now, me*2]
 
 
-samples = NTPMonitor(1, 'time.google.com', 3, 50, .999999999999)
+samples = NTPMonitor(1, 'time.google.com', 3, 50, .9999999)
 
 
 width = 22
 print(tableprint.header(['Last Offset (us)', 'Last Delay', 'Mean Offset (us)', 'RMS Offset', 'Offset St. Dev. (us)', 'Interval Width (us)'], style='clean', width=width))
 
-print(tableprint.row([samples.last.offset*1e+6, samples.status().delay, samples.mean*1e+6, samples.rms_offset, samples.stdev*1e+6, samples.tt_interval()[3]*1e6], width=width, style='clean'))
+print(tableprint.row([samples.last.offset*1e+6, samples.status().delay, samples.mean*1e+6, samples.rms_offset*1e6, samples.stdev*1e+6, samples.tt_interval()[3]*1e6], width=width, style='clean'))
 
 last_tti = None
 while True:
     now = time.time()
     if now > samples.last_computed_sys_time + samples.poll_freq:
         samples.poll()
-        print(tableprint.row([samples.last.offset * 1e+6, samples.status().delay, samples.mean * 1e+6, samples.rms_offset, samples.stdev * 1e+6, samples.tt_interval()[3]*1e6], width=width, style='clean'))
+        print(tableprint.row([samples.last.offset * 1e+6, samples.status().delay, samples.mean * 1e+6, samples.rms_offset*1e6, samples.stdev * 1e+6, samples.tt_interval()[3]*1e6], width=width, style='clean'))
 
     tti = samples.tt_interval()
+    # print(tti)
     # print(abs(tti[1]-tti[2]))
-
 
     if not last_tti:
         last_tti = tti
